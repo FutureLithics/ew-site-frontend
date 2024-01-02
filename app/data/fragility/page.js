be1ead77f9079629b7e3b9666106a6c2a81d8eb7
Chad R Denaux <105078717+FutureLithics@users.noreply.github.com>
2024-01-02 13:46:51 -0500

    Revert "Staging"

--- app/data/fragility/page.js
+++ app/data/fragility/page.js
@@ -1,19 +1,11 @@
-import { Suspense } from "react";
-import { fetchFragilityDataSet } from "../../utils/api";
-import FragilityContext from './FragilityContext';
-import "rsuite/dist/rsuite-no-reset.min.css";
-import Loader from "@/app/_components/Loader";
-import ChoroplethMap from "../../_components/Charts/ChoroplethMap";
-
 const Fragility = async () => {
-
-    return (
-        <Suspense fallback={<Loader />}>
-            <main className="data-charts w-screen flex min-h-screen flex-col items-center justify-between p-12">
-                <FragilityContext />
-            </main>  
-        </Suspense>          
-    );
+  return (
+    <main className="w-screen flex min-h-screen flex-col items-center justify-between p-24">
+      <p>
+        This Page will render a visualization charting microeconomic fragility.
+      </p>
+    </main>
+  );
 };
 
 export default Fragility;
