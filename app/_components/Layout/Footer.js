5a594e71f91ac3274f9cde3d8bcbeb8c7cf3f365
futurelithics <chad.denaux@gmail.com>
2024-01-13 10:58:27 -0500

    minor responsive change

--- app/_components/Layout/Footer.js
+++ app/_components/Layout/Footer.js
@@ -4,16 +4,16 @@ const Footer = () => {
   return (
     <div>
       <div className="absolute bottom-0 bg-baseGreen w-screen text-white grid grid-cols-1 sm:grid-cols-3">
-        <div className="p-4 m-8">
+        <div className="p-4 m-16 sm:my-8 sm:mx-3">
           <h2 className="font-serif font-extrabold content-center text-xl">
             Emily Williams
           </h2>
           <p className="font-bold content-center text-lg">Household Finance</p>
         </div>
-        <div className="p-4 m-8">
+        <div className="p-4 m-16 sm:mx-3 sm:my-8">
           <TaxonomyList />
         </div>
-        <div className="p-4 m-8"></div>
+        <div className="p-4 m-16 sm:mx-3 sm:my-8"></div>
       </div>
     </div>
   );
