91d5d21c55e85e7cd0d149401a1cb63b2cf01330
futurelithics <chad.denaux@gmail.com>
2024-01-13 11:02:42 -0500

    responsive change

--- app/_components/Layout/Footer.js
+++ app/_components/Layout/Footer.js
@@ -2,7 +2,7 @@ import TaxonomyList from "./widgets/TaxonomyList";
 
 const Footer = () => {
   return (
-    <div>
+    <div className="mt-8">
       <div className="absolute bottom-0 bg-baseGreen w-screen text-white grid grid-cols-1 md:grid-cols-3">
         <div className="p-4 flex justify-center md:justify-start sm:my-8 sm:mx-3">
 			<div>
