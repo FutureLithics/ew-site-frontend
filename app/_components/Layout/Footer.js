50c4078aabdb0399803cf35dd28271619a6fbeed
futurelithics <chad.denaux@gmail.com>
2024-01-13 11:02:08 -0500

    responsive change

--- app/_components/Layout/Footer.js
+++ app/_components/Layout/Footer.js
@@ -3,17 +3,25 @@ import TaxonomyList from "./widgets/TaxonomyList";
 const Footer = () => {
   return (
     <div>
-      <div className="absolute bottom-0 bg-baseGreen w-screen text-white grid grid-cols-1 sm:grid-cols-3">
-        <div className="p-4 m-16 sm:my-8 sm:mx-3">
-          <h2 className="font-serif font-extrabold content-center text-xl">
-            Emily Williams
-          </h2>
-          <p className="font-bold content-center text-lg">Household Finance</p>
+      <div className="absolute bottom-0 bg-baseGreen w-screen text-white grid grid-cols-1 md:grid-cols-3">
+        <div className="p-4 flex justify-center md:justify-start sm:my-8 sm:mx-3">
+			<div>
+				<h2 className="font-serif font-extrabold content-center text-xl">
+					Emily Williams
+				</h2>
+				<p className="font-bold content-center text-lg">Household Finance</p>				
+			</div>
         </div>
-        <div className="p-4 m-16 sm:mx-3 sm:my-8">
-          <TaxonomyList />
+        <div className="p-4 flex justify-center sm:mx-3 sm:my-8">
+			<div>
+		          <TaxonomyList />				
+			</div>
         </div>
-        <div className="p-4 m-16 sm:mx-3 sm:my-8"></div>
+        <div className="p-4 flex justify-center sm:mx-3 sm:my-8">
+			<div>
+
+			</div>
+		</div>
       </div>
     </div>
   );
